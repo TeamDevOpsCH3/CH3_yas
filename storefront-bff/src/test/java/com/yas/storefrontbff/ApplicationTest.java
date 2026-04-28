114efb2828f9fc90c6176a8cb68357851a2d3bc1
TonKnight <tanhiep24135@gmail.com>
2026-04-28 17:11:31 +0700

    test: add CI-safe context load test for storefront-bff

---
+++ storefront-bff/src/test/java/com/yas/storefrontbff/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.yas.storefrontbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @MockBean
+    ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    @Test
+    void contextLoads() {
+        // Verify Spring application context loads successfully
+    }
+}
