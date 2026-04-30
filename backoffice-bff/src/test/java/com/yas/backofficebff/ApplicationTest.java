9bebb81521e01ad037fc964a0d8c090c7ddb981a
TonKnight <tanhiep24135@gmail.com>
2026-04-30 19:36:18 +0700

    test(backoffice-bff): Trigger pipeline service

--- backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTest.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 
-//Trigger pipeline
+//Trigger pipeline again
 @SpringBootTest
 class ApplicationTest {
 
